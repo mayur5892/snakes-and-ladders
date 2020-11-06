2e91a2e769648ce0acc82101f09ca9e7723c7eff
mapandey@affinionds.com <mapandey@tavisca.com>
2020-11-06 19:55:08 +0530

    move player implementation

--- src/snakes_and_ladders/core.clj
+++ src/snakes_and_ladders/core.clj
@@ -1,1 +1,19 @@
 (ns snakes-and-ladders.core)
+
+(def players-position (atom {:player1 0}))
+
+(defn- fetch-player-current-position [player-id]
+  (@players-position player-id))
+
+(defn move-player [current-position dice-throw]
+  (let [next-position (+ current-position dice-throw)]
+    (if (> next-position 100)
+      current-position
+      next-position)))
+
+(defn dice-throw []
+  4)
+
+(defn play-next [player-id]
+  (let [current-position (fetch-player-current-position player-id)
+        next-position (move-player current-position (dice-throw))]))
