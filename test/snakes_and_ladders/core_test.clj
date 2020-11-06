79a5501fa942cab88ca7cc7949f7dd28c0d64917
mapandey@affinionds.com <mapandey@tavisca.com>
2020-11-06 19:53:37 +0530

    move player test

--- test/snakes_and_ladders/core_test.clj
+++ test/snakes_and_ladders/core_test.clj
@@ -2,6 +2,12 @@
   (:require [clojure.test :refer :all]
             [snakes-and-ladders.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest move-player-test
+  (testing " a player should move from initial position by the number on dice throw"
+    (is (= 6 (move-player 0 6))))
+
+  (testing " a player should move from current position by the number on dice throw"
+    (is (= 52 (move-player 50 2))))
+
+  (testing "a player should remain in current position if move exceed 100"
+    (is (= 97 (move-player 97 6)))))
