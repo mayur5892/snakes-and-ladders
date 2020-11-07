9a822e4888226223e1d562f26a2b9c259c908630
mapandey@affinionds.com <mapandey@tavisca.com>
2020-11-07 15:58:31 +0530

    making game ready for configurable number of plays

--- README.md
+++ README.md
@@ -2,5 +2,53 @@
 
 Snakes and Ladders console game utility
 
+## pre-requites
+Java
+
+lein
+
 ## Usage
+
+To start game for 15 turns
+
+``` lein run 15```
+
+TO start repl
+
+```lein repl ```
+
+snakes-and-ladders>lein repl
+nREPL server started on port 63480 on host 127.0.0.1 - nrepl://127.0.0.1:63480
+REPL-y 0.4.3, nREPL 0.6.0
+Clojure 1.10.1
+### adding snakes through Repl
+snakes-and-ladders.core=> (add-snake 40 10)
+
+{40 10}
+
+snakes-and-ladders.core=> (add-snake 60 15)
+
+{60 15}
+
+### Fetching snakes configuration
+snakes-and-ladders.core=> @snakes-position
+
+{40 10, 60 15}
+
+### Starting game inside repl
+snakes-and-ladders.core=> (start-game 1)
+
+Press 1 to play with Normal Dice
+
+Press 2 to play with Crooked Dice
+
+2
+
+dice result for this turn  2
+
+Player moved to position:  2  after  1 turns
+
+nil
+
+
 
--- src/snakes_and_ladders/core.clj
+++ src/snakes_and_ladders/core.clj
@@ -10,29 +10,62 @@
   (swap! players-position assoc id position)
   position)
 
+(defn- save-snake [start end]
+  (let [new-snake (hash-map start end)]
+    (swap! snakes-position merge new-snake)
+    new-snake))
+
 (defn move-player [id current-position dice-throw snakes-config]
   (let [next-position (+ current-position dice-throw)]
     (if (> next-position 100)
       current-position
       (update-player-position id (snakes-config next-position next-position)))))
 
-(defn- save-snake [start end]
-  (let [new-snake (hash-map start end)]
-    (swap! snakes-position merge new-snake)
-    new-snake))
-
 (defn add-snake [start end]
   (if (> start end)
     (save-snake start end)
     {}))
 
-(defn throw-crooked-dice []
+(defn- throw-crooked-dice []
   (first (shuffle [2 4 6])))
 
-(defn throw-normal-dice []
+(defn- throw-normal-dice []
   (first (shuffle [1 2 3 4 5 6])))
 
-(defn play-next [player-id]
+(defn- throw-dice [dice-mode]
+  (if (= dice-mode 1)
+    (throw-normal-dice)
+    (throw-crooked-dice)))
+
+(defn- play-next-turn [player-id dice-result]
+  (println "dice result for this turn " dice-result)
   (let [current-position (fetch-player-current-position player-id)
-        snakes-config (@snakes-position)
-        next-position (move-player :player1 current-position (dice-throw) snakes-config)]))
+        snakes-config @snakes-position]
+    (move-player player-id current-position dice-result snakes-config)))
+
+(defn- prompt [message]
+  (println message)
+  (read-line))
+
+(defn- select-dice []
+  (try
+    (let [user-input (Integer/parseInt (prompt "Press 1 to play with Normal Dice \nPress 2 to play with Crooked Dice"))]
+      (if (#{1 2} user-input)
+        user-input
+        (do
+          (println "Invalid Input. Press 1 or 2")
+          (select-dice))
+        ))
+    (catch Exception _
+      (println "Characters are not allowed. Press 1 or 2")
+      (select-dice))))
+
+(defn- start-game [number-of-plays]
+  "Starting Snakes and Ladders Game for 1 Player"
+  (let [dice-mode (select-dice)]
+    (dotimes [_ number-of-plays]
+      (play-next-turn :player1 (throw-dice dice-mode)))
+    (println "Player moved to position: " (@players-position :player1) " after " number-of-plays "turns")))
+
+(defn -main [number-of-plays]
+  (start-game (Integer/parseInt number-of-plays)))
